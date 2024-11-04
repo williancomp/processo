00b66e395f8d64d34df02f32a9fe1702e75566b3
Willian Garcias <williangarcias10@gmail.com>
2024-11-04 18:08:11 -0300

    formatacao 2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# Meu Projeto
+
 [![MeuPipeline](https://github.com/williancomp/processo/actions/workflows/main.yml/badge.svg)](https://github.com/williancomp/processo/actions/workflows/main.yml)
+
+
+Descrição do projeto...
--- components/HelloWorld.spec.ts
+++ components/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from "vitest";
-import { mount } from "@vue/test-utils";
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
 
-import HelloWorld from "./HelloWorld.vue";
+import HelloWorld from './HelloWorld.vue'
 
-describe("HelloWorld", () => {
-  it("component renders Hello world properly", () => {
-    const wrapper = mount(HelloWorld);
-    expect(wrapper.text()).toContain("Hello world");
-  });
-});
+describe('HelloWorld', () => {
+  it('component renders Hello world properly', () => {
+    const wrapper = mount(HelloWorld)
+    expect(wrapper.text()).toContain('Hello world')
+  })
+})
